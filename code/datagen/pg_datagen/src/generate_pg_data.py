98a78292979ffcf762d493ed444d1805b24ff203
Aleksandr Shapoval <a-v.shapowal@yandex.ru>
2024-12-29 19:31:51 +0300

    v2 - fix pg_generator, add airflow, upgraded setup for kafka&zk, changed dbs and infra structure

--- code/datagen/pg_datagen/src/generate_pg_data.py
+++ code/datagen/pg_datagen/src/generate_pg_data.py
@@ -1,10 +1,13 @@
 import random
 from datetime import datetime, timedelta
 from typing import Callable
+import logging
+import os
 
 from faker import Faker
 from faker_commerce import Provider as CommerceProvider
 from psycopg2.extensions import cursor
+from psycopg2 import OperationalError, DatabaseError
 
 from database import PGConnector
 
@@ -19,16 +22,43 @@ class DataGenerator:
         Args:
             db_connector (PGConnector): Объект для управления соединениями с СУБД.
         """
-        self.db_connector = db_connector
-        self.fake = Faker()
-        self.fake.add_provider(CommerceProvider)
+        self.__db_connector = db_connector
+        self.__fake = Faker()
+        self.__fake.add_provider(CommerceProvider)
 
-        self.user_ids = []
-        self.product_ids = []
-        self.order_ids = []
-        self.category_ids = []
+        self.__user_ids = []
+        self.__product_ids = []
+        self.__order_ids = []
+        self.__category_ids = []
 
-    def generate_users(self, cur: cursor, num_users: int = 100) -> None:
+        self.__initialize_parameters_from_env()
+
+        logging.basicConfig(level=logging.INFO)
+        self.__logger = logging.getLogger(__name__)
+
+    def __initialize_parameters_from_env(self):
+        """Инициализация параметров из переменных окружения."""
+        self.__num_users = int(os.getenv('PG_DATAGEN_NUM_USERS', 5000))
+        self.__num_products = int(os.getenv('PG_DATAGEN_NUM_PRODUCTS', 500))
+        self.__num_orders = int(os.getenv('PG_DATAGEN_NUM_ORDERS', 10000))
+        self.__num_order_details = int(os.getenv('PG_DATAGEN_NUM_ORDER_DETAILS', 40000))
+        self.__num_categories = int(os.getenv('PG_DATAGEN_NUM_CATEGORIES', 40000))
+    
+    def __table_is_empty(self, cur, table_name):
+        """Проверка, что таблица пуста.
+
+        Args:
+            cur (cursor): Объект курсора для выполнения SQL-запросов.
+            table_name (str): Название таблицы.
+
+        Returns:
+            bool: True, если таблица пуста, иначе False.
+        """
+        cur.execute(f"SELECT COUNT(*) FROM {table_name}")
+        count = cur.fetchone()[0]
+        return count == 0
+
+    def __generate_users(self, cur: cursor, num_users: int = 100) -> None:
         """Генерация данных для таблицы users.
 
         Args:
@@ -36,13 +66,13 @@ class DataGenerator:
             num_users (int): Количество пользователей для генерации.
         """
         for _ in range(num_users):
-            first_name = self.fake.first_name()
-            last_name = self.fake.last_name()
+            first_name = self.__fake.first_name()
+            last_name = self.__fake.last_name()
 
-            email = self.fake.email()
-            phone = self.fake.phone_number()
+            email = self.__fake.email()
+            phone = self.__fake.phone_number()
 
-            registration_date = self.fake.date_time_between(start_date='-1y', end_date='now')
+            registration_date = self.__fake.date_time_between(start_date='-1y', end_date='now')
             loyalty_status = random.choice(["Gold", "Silver", "Bronze"])
 
             cur.execute(
@@ -50,9 +80,9 @@ class DataGenerator:
                 (first_name, last_name, email, phone, registration_date, loyalty_status)
             )
             user_id = cur.fetchone()[0]
-            self.user_ids.append(user_id)
+            self.__user_ids.append(user_id)
 
-    def generate_product_categories(self, cur: cursor, num_categories: int = 10) -> None:
+    def __generate_product_categories(self, cur: cursor, num_categories: int = 10) -> None:
         """Генерация данных для таблицы product_categories.
 
         Args:
@@ -67,22 +97,22 @@ class DataGenerator:
                 (category, None)
             )
             category_id = cur.fetchone()[0]
-            self.category_ids.append(category_id)
+            self.__category_ids.append(category_id)
 
         for _ in range(num_categories):
-            name = self.fake.words(nb=1)
+            name = self.__fake.words(nb=1)
 
             # 50/50, что получится подкатегория
-            parent_category_id = random.choice(self.category_ids) if self.category_ids and random.choice([True, False]) else None
+            parent_category_id = random.choice(self.__category_ids) if self.__category_ids and random.choice([True, False]) else None
 
             cur.execute(
                 "INSERT INTO product_categories (name, parent_category_id) VALUES (%s, %s) RETURNING category_id",
                 (name, parent_category_id)
             )
             category_id = cur.fetchone()[0]
-            self.category_ids.append(category_id)
+            self.__category_ids.append(category_id)
 
-    def generate_products(self, cur: cursor, num_products: int = 100) -> None:
+    def __generate_products(self, cur: cursor, num_products: int = 100) -> None:
         """Генерация данных для таблицы products.
 
         Args:
@@ -90,8 +120,8 @@ class DataGenerator:
             num_products (int): Количество товаров для генерации.
         """
         for _ in range(num_products):
-            name = self.fake.ecommerce_name()
-            description = self.fake.paragraph(nb_sentences=2)
+            name = self.__fake.ecommerce_name()
+            description = self.__fake.paragraph(nb_sentences=2)
 
             category_id = random.randint(1, 10)
             price = round(random.uniform(10.0, 100.0), 2)
@@ -104,9 +134,9 @@ class DataGenerator:
                 (name, description, category_id, price, stock_quantity, creation_date)
             )
             product_id = cur.fetchone()[0]
-            self.product_ids.append(product_id)
+            self.__product_ids.append(product_id)
 
-    def generate_orders(self, cur: cursor, num_orders: int = 100) -> None:
+    def __generate_orders(self, cur: cursor, num_orders: int = 100) -> None:
         """Генерация данных для таблицы orders.
 
         Args:
@@ -114,8 +144,8 @@ class DataGenerator:
             num_orders (int): Количество заказов для генерации.
         """
         for _ in range(num_orders):
-            user_id = random.choice(self.user_ids)
-            order_date = self.fake.date_time_between(start_date='-1y', end_date='now')
+            user_id = random.choice(self.__user_ids)
+            order_date = self.__fake.date_time_between(start_date='-1y', end_date='now')
             
             total_amount = round(random.uniform(50.0, 500.0), 2)
             status = random.choice(["Pending", "Completed"])
@@ -126,9 +156,9 @@ class DataGenerator:
                 (user_id, order_date, total_amount, status, delivery_date)
             )
             order_id = cur.fetchone()[0]
-            self.order_ids.append(order_id)
+            self.__order_ids.append(order_id)
 
-    def generate_order_details(self, cur: cursor, num_order_details: int = 100) -> None:
+    def __generate_order_details(self, cur: cursor, num_order_details: int = 100) -> None:
         """Генерация данных для таблицы order_details.
 
         Args:
@@ -136,8 +166,8 @@ class DataGenerator:
             num_order_details (int): Количество деталей заказов для генерации.
         """
         for _ in range(num_order_details):
-            order_id = random.choice(self.order_ids)
-            product_id = random.choice(self.product_ids)
+            order_id = random.choice(self.__order_ids)
+            product_id = random.choice(self.__product_ids)
 
             quantity = random.randint(1, 10)
             price_per_unit = round(random.uniform(10.0, 100.0), 2)
@@ -148,7 +178,7 @@ class DataGenerator:
                 (order_id, product_id, quantity, price_per_unit, total_price)
             )
 
-    def execute_generation_task(self, cur: cursor, task_name: str, generate_method: Callable, num_elements: int = 100) -> None:
+    def __execute_generation_task(self, cur: cursor, table_name: str, generate_method: Callable, num_elements: int = 100) -> None:
         """Выполнение задачи генерации
 
         Args:
@@ -157,47 +187,52 @@ class DataGenerator:
             task_name (str): Название таски
             num_elements (int, optional): Количество элементов, которые нужно сгенерировать. Defaults to 100.
         """
-        print(f"Generating data ({num_elements} rows) for {task_name}...")
-        generate_method(cur, num_elements)
-        print(f"Generated {num_elements} rows")
+        if not self.__table_is_empty(cur, table_name):
+            self.__logger.info(f"Table {table_name} already contains data. Skipping...")
+            return
+
+        self.__logger.info(f"Generating data ({num_elements} rows) for {table_name}...")
+
+        try:
+            generate_method(cur, num_elements)
+            self.__logger.info(f"Successfully generated {num_elements} rows for {table_name}.")
+        except (OperationalError, DatabaseError)  as e:
+            self.__logger.info(f"Error generating data: {e}")
 
     def orchestrate_population(self) -> None:
         """Оркестрация процесса генерации данных для всех таблиц."""
         
         with (
-            self.db_connector.get_conn() as conn,
+            self.__db_connector.get_conn() as conn,
             conn.cursor() as cur
         ):
             # Юзеры
-            self.execute_generation_task(cur, 'users', self.generate_users, 5000)
+            self.__execute_generation_task(cur, 'users', self.__generate_users, 5000)
 
             # Категории
-            self.execute_generation_task(cur, 'product categories', self.generate_product_categories, 500)
+            self.__execute_generation_task(cur, 'product_categories', self.__generate_product_categories, 500)
 
             # Продукты
-            self.execute_generation_task(cur, 'products', self.generate_products, 10000)
+            self.__execute_generation_task(cur, 'products', self.__generate_products, 10000)
 
             # Заказы
-            self.execute_generation_task(cur, 'orders', self.generate_orders, 40000)
+            self.__execute_generation_task(cur, 'orders', self.__generate_orders, 40000)
 
             # Детали заказов
-            self.execute_generation_task(cur, 'order details', self.generate_order_details, 40000)
+            self.__execute_generation_task(cur, 'order_details', self.__generate_order_details, 40000)
 
             # Все или ничего
             conn.commit()
 
-            print("Everything seems fine, session commited")
+            self.__logger.info("Session committed!")
 
 
 def main():
     """Основная функция для генерации данных."""
     pg_connector = PGConnector()
-    data_generator = DataGenerator(pg_connector)
 
-    try:
-        data_generator.orchestrate_population()
-    except Exception as e:
-        print(f"Error generating data: {e}")
+    data_generator = DataGenerator(pg_connector)
+    data_generator.orchestrate_population()
 
 
 if __name__ == "__main__":
---
+++ infra/airflow/init/setup_airflow.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Запуск Airflow
+airflow db migrate
+
+# Создание юзеров
+airflow users create \
+    --username admin \
+    --password admin \
+    --firstname Peter \
+    --lastname Parker \
+    --role Admin \
+    --email spiderman@superhero.org
+
+# Создание коннектов
+airflow connections add \
+    --conn-type 'postgres' \
+    --conn-host "$POSTGRESQL_APP_HOST" \
+    --conn-login "$POSTGRESQL_APP_USER" \
+    --conn-password "$POSTGRESQL_APP_PASSWORD" \
+    --conn-schema "$POSTGRESQL_APP_DB" \
+    --conn-port '5432' \
+    'python_de_finals_postges'
+
+airflow connections add \
+    --conn-type 'mysql' \
+    --conn-host "$MYSQL_HOST" \
+    --conn-login "$MYSQL_APP_USER" \
+    --conn-password "$MYSQL_APP_PASSWORD" \
+    --conn-schema "$MYSQL_DATABASE" \
+    --conn-port '3306' \
+    'python_de_finals_mysql'
+
+airflow version
